b37ad6e057094a74dac9872d8bd37b702f8e67fa
shulabh234 <shulabhdixit234@gmail.com>
2023-06-24 17:55:59 +0530

    first.js file added to feature branch 2

---
+++ first.js
@@ -0,0 +1,10 @@
+function padLeft(padding, input) {
+    if (typeof padding === "string") {
+        return padding + input;
+    }
+    else {
+        return String(padding) + input;
+    }
+}
+console.log(padLeft(100, 'px'));
+console.log("testing git")
