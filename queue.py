4196208801a43ea2c133e50bb898659a4c44d729
arpitji <arpitdixit1510@gmail.com>
2024-08-24 14:07:21 +0530

    feat: updated stack and add queue

---
+++ queue.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+
+class Queue:
+    def __init__(self,size=5):
+        self.size = size
+        self.queue = []
--- stack.py
+++ stack.py
@@ -3,4 +3,9 @@ class Stack:
     def __init__(self,size=5):
         self.size = size
         self.stack = []
-
+    
+    def isEmpty(self):
+        if self.size == len(self.stack):
+            return False
+        else:
+            return True
